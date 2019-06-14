e84a087e94c6197fbb5656ec8e2a81d9b583ae27
cvilleger <villeger.c@gmail.com>
2019-06-14 02:17:21 +0200

    init2

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,7 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
---
+++ src/Provider/UserProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Provider;
+
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserProvider implements OAuthAwareUserProviderInterface
+{
+
+    /**
+     * Loads the user by a given UserResponseInterface object.
+     *
+     * @param UserResponseInterface $response
+     *
+     * @return UserInterface
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     */
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response): ?UserInterface
+    {
+        // TODO: Implement loadUserByOAuthUserResponse() method.
+    }
+}
